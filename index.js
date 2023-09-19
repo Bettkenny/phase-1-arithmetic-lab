148ea03e0f9046d418d056de622b87b1db5d3342
YOUR NAME <YOUR@EMAIL.com>
2023-09-19 10:45:55 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
+const num2 =2;
+const multiply = (num1*num2);
+console.log(multiply);{
+  const random = Math. floor(Math.random())+10 + 1;
+  console.log(random)
+}
+{
+  const num3 = 20;
+  const num4 = 5;
+  const mod = (num3/num4);
+  console.log(mod);
+}
+{
+  const num5 = 20;
+  const num6 = 18;
+  const num7 = 1;
+  const num8 = 7;
+  const max = Math.max(num5, num6, num7, num8);
+  console.log(max);
+}
 
-// Write your code here
